2cfdb78b8dd3f3a3f83c943286c967836c4abbd9
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 12:41:19 +0300

    feat: enable bottom nav routing on progress screen

--- app/src/main/java/com/example/profitness/Your_progress.java
+++ app/src/main/java/com/example/profitness/Your_progress.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.profitness.navigation.BottomNavHelper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Your_progress extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_progress);
+
+        BottomNavigationView nav = findViewById(R.id.bottom_navigation);
+        BottomNavHelper.setup(this, nav, R.id.nav_progress);
     }
 }
