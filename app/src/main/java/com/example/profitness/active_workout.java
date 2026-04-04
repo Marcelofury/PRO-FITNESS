101e531285955a025a9f4a032092e02bbc536aa5
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 11:27:04 +0300

    chore: update app/src/main/java/com/example/profitness/active_workout.java

---
+++ app/src/main/java/com/example/profitness/active_workout.java
@@ -0,0 +1,14 @@
+package com.example.profitness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class active_workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_workout);
+    }
+}
