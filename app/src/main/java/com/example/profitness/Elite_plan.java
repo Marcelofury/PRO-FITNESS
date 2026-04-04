d6de31be4f4b096e25d3a53b62915f612c217a57
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 11:26:54 +0300

    chore: update app/src/main/java/com/example/profitness/Elite_plan.java

---
+++ app/src/main/java/com/example/profitness/Elite_plan.java
@@ -0,0 +1,14 @@
+package com.example.profitness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Elite_plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elite_plan);
+    }
+}
