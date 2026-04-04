75823f18ca048593829d2203080fd39c747643f6
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 11:27:02 +0300

    chore: update app/src/main/java/com/example/profitness/Your_progress.java

---
+++ app/src/main/java/com/example/profitness/Your_progress.java
@@ -0,0 +1,14 @@
+package com.example.profitness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Your_progress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_progress);
+    }
+}
