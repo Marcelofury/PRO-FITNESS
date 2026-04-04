8219af5b92209b883c6761f64b656429e84563cc
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 11:27:12 +0300

    chore: update app/src/main/java/com/example/profitness/hydration_tracker.java

---
+++ app/src/main/java/com/example/profitness/hydration_tracker.java
@@ -0,0 +1,14 @@
+package com.example.profitness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hydration_tracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hydration_tracker);
+    }
+}
