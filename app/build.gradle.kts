b5faf6a16ce16fd5741594c7f02f95d83810c25a
Marcelofury <marcelofury13579@gmail.com>
2026-04-04 12:12:08 +0300

    build: add Android app networking dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
     implementation(libs.androidx.activity)
+    implementation(libs.okhttp)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
